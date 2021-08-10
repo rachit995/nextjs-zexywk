0e810ac2114e58a8acee28994e8727aac6c3e26b
rachit995 <rachit995@rocketmail.com>
2021-08-10 17:36:40 +0530

    2 files modified

--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,50 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 
 export default function Home() {
+  const Item = ({ id }) => <div>I am item: {id}</div>;
+  const refs = useRef({});
+  const gridRef = useRef();
+
+  if (Object.keys(refs.current).length !== items.length) {
+    items.forEach(({ id }) => {
+      refs.current[id] = refs.current[id] || createRef();
+    });
+  }
+
+  useEffect(() => {
+    gridRef.current =
+      gridRef.current ||
+      GridStack.init(
+        {
+          float: true
+        },
+        '.controlled'
+      );
+    const grid = gridRef.current;
+    grid.batchUpdate();
+    grid.removeAll(false);
+    items.forEach(({ id }) => grid.makeWidget(refs.current[id].current));
+    grid.commit();
+  }, [items]);
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-        </a>
-      </footer>
+    <div>
+      <button onClick={addItem}>Add new widget</button>
+      <div className={`grid-stack controlled`}>
+        {items.map((item, i) => {
+          return (
+            <div
+              ref={refs.current[item.id]}
+              key={item.id}
+              className={'grid-stack-item'}
+            >
+              <div className="grid-stack-item-content">
+                <Item {...item} />
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
